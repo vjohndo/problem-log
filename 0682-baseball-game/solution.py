a69aa6bf9ce928f72d4e6aa177eeb1ce28d12d8a
vjohndo <v.john.do@outlook.com>
2023-03-01 19:56:11 +1100

    0682 reviewed

--- 0682-baseball-game/solution.py
+++ 0682-baseball-game/solution.py
@@ -5,10 +5,12 @@ class Solution:
 
         for operation in operations: 
             if operation == "+":
+                # On review can just include these in the brackets
                 previous = score[-1]
                 penultimate = score[-2]
                 score.append(previous + penultimate)
             elif operation == "D":
+                # On review can just include these in the brackets
                 previous = score[-1]
                 score.append(previous * 2)
             elif operation == "C":
