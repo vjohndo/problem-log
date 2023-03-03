2b78817e17e9693eacc3575b079784d0f4b330ef
vjohndo <v.john.do@outlook.com>
2023-03-04 00:29:19 +1100

    0095 completed:

---
+++ 0095-unique-binary-search-tree-II/solution.py
@@ -0,0 +1,63 @@
+class SolutionIterative(object):
+    def inorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        result, stack = [], []
+
+        stack.append(root)
+
+        while len(stack) > 0:
+            print(stack)
+            current = stack.pop()
+            if current is None:
+                continue
+
+            if type(current) == int:
+                result.append(current)
+                continue
+
+            stack.append(current.right)
+            stack.append(current.val)
+            stack.append(current.left)
+    
+        return result
+
+class Solution(object):
+    def inorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        result = []
+
+        def inorder(root, result):
+            if not root:
+                return
+            
+            inorder(root.left, result)
+            result.append(root.val)
+            inorder(root.right, result)
+
+        inorder(root, result)
+        return result
+
+class Solution(object):
+    def inorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        result = []
+        self.inorder(root, result)
+        return result
+
+
+    def inorder(self, root, result):
+        if not root:
+            return
+        
+        self.inorder(root.left, result)
+        result.append(root.val)
+        self.inorder(root.right, result)
