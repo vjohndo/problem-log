8396515654f4dc51310dc82874749db9514ae772
vjohndo <v.john.do@outlook.com>
2023-03-03 11:56:38 +1100

    0278 0875 reviewed

--- 0278-first-bad-version/solution.py
+++ 0278-first-bad-version/solution.py
@@ -7,6 +7,27 @@ class Solution(object):
 
         left = 1
         right = n
+        first_bad = right
+
+        while left <= right:
+            mid = (left + right) // 2
+            if isBadVersion(mid):
+                first_bad = mid
+                right = mid - 1
+            else:
+                left = mid + 1
+        
+        return first_bad
+
+class Solution(object):
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+
+        left = 1
+        right = n
 
         while left < right:
             mid = (left + right) // 2
---
+++ 0875-koko-eating-banans/solution.py
@@ -0,0 +1,63 @@
+class Solution(object):
+    def minEatingSpeed(self, piles, h):
+        """
+        :type piles: List[int]
+        :type h: int
+        :rtype: int
+        """
+    
+        def isEnoughTime(piles, h, k):
+            total_visits = 0
+
+            for bananas in piles:
+                total_visits += - (bananas // -k)
+
+            if total_visits <= h:
+                return True
+            
+            return False
+
+        left = 1
+        right = max(piles)
+
+        while left < right:
+            mid = (left + right) // 2
+            if isEnoughTime(piles, h, mid):
+                right = mid
+            else:
+                left = mid + 1
+        
+        return right
+
+class SolutionAttempt1(object):
+    def minEatingSpeed(self, piles, h):
+        """
+        :type piles: List[int]
+        :type h: int
+        :rtype: int
+        """
+    
+        def isEnoughTime(piles, h, k):
+            visits = [-(bananas// -k) for bananas in piles]
+            total_visits = sum(visits)
+
+            if total_visits <= h:
+                return True
+            
+            return False
+
+        # print(isEnoughTime([3,6,7,11], 8, 3))
+        # print(isEnoughTime([3,6,7,11], 8, 4))
+        # print(isEnoughTime([3,6,7,11], 8, 5))
+
+        left = 1
+        right = max(piles)
+
+        while left < right:
+            mid = (left + right) // 2
+            if isEnoughTime(piles, h, mid):
+                left = mid # INCORRECT HERE ... should be right = mid
+            else:
+                right = mid - 1 # INCORRECT HERE ... should be left = mid + 1
+        
+        return left
