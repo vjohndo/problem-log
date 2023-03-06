8b835e8446ed9088d5f5b4de951d9407056d9976
vjohndo <v.john.do@outlook.com>
2023-03-06 16:23:42 +1100

    0450 revised

--- 0450-delete-node-in-bst/solution.py
+++ 0450-delete-node-in-bst/solution.py
@@ -1,3 +1,31 @@
+class SolutionMaxOfLeftSubtree:
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        if root is None:
+            return None
+        
+        if root.val > key:
+            root.left = self.deleteNode(root.left, key)
+        elif root.val < key:
+            root.right = self.deleteNode(root.right, key)
+        else:
+            if root.right is None:
+                return root.left
+            elif root.left is None:
+                return root.right
+            else:
+                max_left = self.findMaxNode(root.left)
+                root.val = max_left.val
+                root.left = self.deleteNode(root.left, max_left.val)
+                
+        return root
+
+
+    def findMaxNode(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        current = root
+        while current is not None and current.right is not None:
+            current = current.right
+        return current
+
 class Solution(object):
     def deleteNode(self, root, key):
         """
