a020c55b5d37f030a626ac094a14e5afa2c5ad47
vjohndo <v.john.do@outlook.com>
2023-03-08 13:57:40 +1100

    0230 revised

--- 0230-kth-smallest-element-in-bst/solution.py
+++ 0230-kth-smallest-element-in-bst/solution.py
@@ -5,11 +5,55 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
+        stack = []
+        current = root
+
+        while current or root:
+            while current:
+                stack.append(current)
+                current = current.left
+            current = stack.pop()
+            k -= 1
+            if k == 0:
+                return current.val
+            current = current.right
+
+class Solution(object):
+    def kthSmallest(self, root, k):
+        """
+        :type root: TreeNode
+        :type k: int
+        :rtype: int
+        """
+        stack = []
+        current = root
+        count = 0
+
+        while current or root:
+            while current:
+                stack.append(current)
+                current = current.left
+
+            current = stack.pop()
+            count += 1
+
+            if count == k:
+                return current.val
+
+            current = current.right
+
+class Solution(object):
+    def kthSmallest(self, root, k):
+        """
+        :type root: TreeNode
+        :type k: int
+        :rtype: int
+        """
         def inorder(root, res):
             if not root:
                 return None
 
-            if len(res) < k:
+            if len(res) < k: 
                 inorder(root.left, res)
                 res.append(root.val)
                 inorder(root.right, res)
