86627475999ece89332ea374ed70c52b58dfc997
vjohndo <v.john.do@outlook.com>
2023-03-11 11:10:19 +1100

    0078 revised

--- 0078-subsets/solution.py
+++ 0078-subsets/solution.py
@@ -1,3 +1,27 @@
+class Solution(object):
+    def subsets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+
+        res = []
+        res.append([])
+
+        def dfs(i, path):
+            if i >= len(nums):
+                return
+            
+            for j in range(i, len(nums)):
+                path.append(nums[j])
+                res.append(list(path))
+                dfs(j + 1, path)
+                path.pop()
+
+        dfs(0, [])
+
+        return res
+
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         res = []
@@ -17,7 +41,7 @@ class Solution:
         findsubsets(0)
         return res
 
-class Solution:
+class SolutionOGUnin:
     def subsets(self, nums: List[int]) -> List[List[int]]:
 
         res = []
@@ -32,6 +56,31 @@ class Solution:
                 path.pop()
                 
         findsubsets(0)
+        return res
+
+class SolutionAddOnlyOnRightTraversal(object):
+    def subsets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+
+        res = []
+        res.append([])
+
+        def dfs(i, path):
+            if i >= len(nums):
+                return
+            
+            path.append(nums[i])
+            res.append(list(path))
+            dfs(i + 1, path)
+            path.pop()
+            dfs(i + 1, path)
+            
+
+        dfs(0, [])
+
         return res
 
 class SolutionAccepted:
