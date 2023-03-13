24a0bb5b90eacce485037ff0bf898594fb0319fb
vjohndo <v.john.do@outlook.com>
2023-03-14 01:01:15 +1100

    0703 revised

--- 0703-kth-largest-element-in-a-stream/solution.py
+++ 0703-kth-largest-element-in-a-stream/solution.py
@@ -1,5 +1,92 @@
-class KthLargestRecursive(object):
+class KthLargestUsingInBuiltClass(object):
 
+    def __init__(self, k, nums):
+        self.k = k
+        self.heap = nums
+        heapq.heapify(self.heap)
+
+        while len(self.heap) > self.k:
+            heapq.heappop(self.heap)
+
+    def add(self, val):
+        if len(self.heap) < self.k:
+            heapq.heappush(self.heap, val)
+        elif val > self.heap[0]:
+            heapq.heapreplace(self.heap, val)
+        
+        return self.heap[0]
+
+class KthLargestHeapified(object):
+    def __init__(self, k, nums):
+        """
+        :type k: int
+        :type nums: List[int]
+        """
+        self.k = k
+        self.heap = nums
+
+        if nums:
+            self.heap.append(self.heap[0])
+        else:
+            self.heap.append(None)
+
+        i = (len(self.heap) - 1) // 2
+        while i >= 1:
+            self.downheap(i)
+            # while i >= 1:
+            #     j = i
+            #     while j * 2 < len(self.heap):
+            #         if (j * 2 + 1 < len(self.heap) and
+            #         self.heap[j * 2 + 1] < self.heap[j * 2] and
+            #         self.heap[j * 2 + 1] < self.heap[j]):
+            #             self.heap[j * 2 + 1], self.heap[j] = self.heap[j], self.heap[j * 2 + 1]
+            #             j = j * 2 + 1
+            #         elif self.heap[j * 2] < self.heap[j]:
+            #             self.heap[j * 2], self.heap[j] = self.heap[j], self.heap[j * 2]
+            #             j = j * 2
+            #         else:
+            #             break
+            #     i -= 1
+            i -= 1
+        
+        while len(self.heap) - 1 > k:
+            self.heap[1] = self.heap.pop()
+            self.downheap(1)
+        
+    def add(self, val):
+        """
+        :type val: int
+        :rtype: int
+        """        
+        if len(self.heap) - 1 < self.k:
+
+            self.heap.append(val)
+            i = len(self.heap) - 1
+            self.upheap(i)
+
+        elif val > self.heap[1]:
+            self.heap[1] = val
+            self.downheap(1)
+
+        return self.heap[1]
+    
+    def upheap(self, j):
+        if j > 1 and self.heap[j] < self.heap[j // 2]:
+            self.heap[j], self.heap[j // 2] = self.heap[j // 2], self.heap[j]
+            self.upheap(j // 2)
+    
+    def downheap(self, j):
+        if j * 2 >= len(self.heap):
+            return
+
+        if j * 2 + 1 < len(self.heap) and self.heap[j * 2 + 1] < self.heap[j * 2] and self.heap[j * 2 + 1] < self.heap[j]:
+            self.heap[j], self.heap[j * 2 + 1] = self.heap[j * 2 + 1], self.heap[j]
+            self.downheap(j * 2 + 1)
+        elif self.heap[j * 2] < self.heap[j]:
+            self.heap[j], self.heap[j * 2] = self.heap[j * 2], self.heap[j]
+            self.downheap(j * 2)
+
+class KthLargestRecursive(object):
     def __init__(self, k, nums):
         """
         :type k: int
