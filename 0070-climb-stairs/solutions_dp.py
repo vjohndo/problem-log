967e50b6869309f981bb6c3fed591f3e12aee3fd
vjohndo <v.john.do@outlook.com>
2023-03-18 01:14:09 +1100

    0070 revised

---
+++ 0070-climb-stairs/solutions_dp.py
@@ -0,0 +1,39 @@
+def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        # bottom up approach
+        def dp(n):
+            if n <= 3:
+                return n
+            vals = [2, 3]
+
+            for i in range(4, n + 1):
+                temp = vals[1]
+                vals[1] += vals[0]
+                vals[0] = temp
+            
+            return vals[1]
+        
+        return dp(n)
+
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        # Topdown, memoisation, caching
+        def dp(n, cache):
+            if n <= 3:
+                return n
+            
+            if n in cache:
+                return cache[n]
+            
+            cache[n] = dp(n-1, cache) + dp(n-2, cache)
+            return cache[n]
+        
+        return dp(n, {})
+    
