01e75cb5bcc0e8f630c606109af73715d7f24cf8
vjohndo <v.john.do@outlook.com>
2023-03-29 23:50:40 +1100

    1844 revised

--- 1844-replace-all-digits-with-characters/solution.py
+++ 1844-replace-all-digits-with-characters/solution.py
@@ -1,3 +1,21 @@
+class SolutionBetter(object):
+    def replaceDigits(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+
+        def shift(char, num):
+            return chr(ord(char) + int(num))
+
+        res = list(s)
+
+        for i in range(len(s)):
+            if i % 2 != 0:
+                res[i] = shift(s[i - 1], s[i])
+
+        return "".join(res)
+
 class Solution(object):
     def replaceDigits(self, s):
         """
@@ -17,3 +35,4 @@ class Solution(object):
                 res.append(shift(s[i - 1], s[i]))
 
         return "".join(res)
+
