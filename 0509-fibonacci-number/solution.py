2b77f28108da7c131399000ab6e11a6b0f16bcbf
vjohndo <v.john.do@outlook.com>
2023-04-07 22:45:18 +1000

    0509 revised

--- 0509-fibonacci-number/solution.py
+++ 0509-fibonacci-number/solution.py
@@ -1,3 +1,19 @@
+class SolutionDP(object):
+    def fib(self, n):
+        if n <= 1:
+            return n
+
+        dp = [0, 1]
+
+        i = 2
+        while i <= n:
+            temp = dp[1]
+            dp[1] += dp[0]
+            dp[0] = temp
+            i += 1
+        
+        return dp[1]
+
 class SolutionLinearRegression:
     def fib(self, n: int) -> int:
         return self.fib_helper(n)[0]
