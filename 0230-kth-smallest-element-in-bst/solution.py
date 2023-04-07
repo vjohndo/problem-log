3c88bda89e52ff45dad72ee1fae84dd474788ffa
vjohndo <v.john.do@outlook.com>
2023-04-07 15:30:59 +1000

    0230 revised

--- 0230-kth-smallest-element-in-bst/solution.py
+++ 0230-kth-smallest-element-in-bst/solution.py
@@ -1,3 +1,67 @@
+class SolutionIterative(object):
+    def kthSmallest(self, root, k):
+        stack = []
+        current = root
+
+        while current or root:
+            while current:
+                stack.append(current)
+                current = current.left
+            current = stack.pop()
+            k -= 1
+            if k == 0:
+                return current.val
+            current = current.right
+
+class Solution(object):
+    def kthSmallest(self, root, k):
+        stack = []
+        current = root
+        count = 0
+
+        while current or root:
+            while current:
+                stack.append(current)
+                current = current.left
+
+            current = stack.pop()
+            count += 1
+
+            if count == k:
+                return current.val
+
+            current = current.right
+
+class Solution(object):
+    def kthSmallest(self, root, k):
+        def inorder(root, res):
+            if not root:
+                return None
+
+            if len(res) < k: 
+                inorder(root.left, res)
+                res.append(root.val)
+                inorder(root.right, res)
+
+            return res
+        
+        return inorder(root, [])[k-1]
+
+class Solution(object):
+    def kthSmallest(self, root, k):
+        res = []
+        def inorder(root):
+            if not root:
+                return root
+
+            inorder(root.left)
+            res.append(root.val)
+            inorder(root.right)
+        
+        inorder(root)
+
+        return res[k - 1]
+
 class SolutionPython3GlobalVariables:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
         res = root.val
@@ -19,89 +83,3 @@ class SolutionPython3GlobalVariables:
         
         dfs(root)
         return res
-
-class Solution(object):
-    def kthSmallest(self, root, k):
-        """
-        :type root: TreeNode
-        :type k: int
-        :rtype: int
-        """
-        stack = []
-        current = root
-
-        while current or root:
-            while current:
-                stack.append(current)
-                current = current.left
-            current = stack.pop()
-            k -= 1
-            if k == 0:
-                return current.val
-            current = current.right
-
-class Solution(object):
-    def kthSmallest(self, root, k):
-        """
-        :type root: TreeNode
-        :type k: int
-        :rtype: int
-        """
-        stack = []
-        current = root
-        count = 0
-
-        while current or root:
-            while current:
-                stack.append(current)
-                current = current.left
-
-            current = stack.pop()
-            count += 1
-
-            if count == k:
-                return current.val
-
-            current = current.right
-
-class Solution(object):
-    def kthSmallest(self, root, k):
-        """
-        :type root: TreeNode
-        :type k: int
-        :rtype: int
-        """
-        def inorder(root, res):
-            if not root:
-                return None
-
-            if len(res) < k: 
-                inorder(root.left, res)
-                res.append(root.val)
-                inorder(root.right, res)
-
-            return res
-        
-        return inorder(root, [])[k-1]
-
-class Solution(object):
-    def kthSmallest(self, root, k):
-        """
-        :type root: TreeNode
-        :type k: int
-        :rtype: int
-        """
-
-        res = []
-
-        def inorder(root):
-            if not root:
-                return root
-
-            inorder(root.left)
-            res.append(root.val)
-            inorder(root.right)
-        
-        inorder(root)
-
-        return res[k - 1]
