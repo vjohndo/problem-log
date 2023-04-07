5a9297396ae75c7fb63666c00b77cc2c38a1c121
vjohndo <v.john.do@outlook.com>
2023-04-07 22:07:32 +1000

    0225 revised

--- 0225-implement-stack-using-queues/solution.py
+++ 0225-implement-stack-using-queues/solution.py
@@ -1,3 +1,24 @@
+class MyStackSimplifiedImplementation(object):
+
+    def __init__(self):
+        self.q = collections.deque()
+
+    def push(self, x):
+        self.q.append(x)
+        for _ in range(len(self.q) - 1):
+            self.q.append(self.q.popleft())
+        
+
+    def pop(self):
+        return self.q.popleft()
+
+    def top(self):
+        return self.q[0]
+        
+
+    def empty(self):
+        return len(self.q) == 0
+
 class MyStack:
     class MyQueue:
         class Node:
