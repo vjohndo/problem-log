7f5a41d303b7dc6b0e9432a5c8c656faad9172cd
vjohndo <v.john.do@outlook.com>
2023-04-11 01:25:22 +1000

    0207 revised

--- 0207-course-schedule/solutions.py
+++ 0207-course-schedule/solutions.py
@@ -35,7 +35,91 @@ class SolutionDFS(object):
 
         return True
 
-class SolutionBFS(object):
+class SolutionRevisedBFS(object):
+    def canFinish(self, numCourses, prerequisites):
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :rtype: bool
+        """
+        
+        adj = {i : [] for i in range(numCourses)}
+        dep = {i : 0 for i in range(numCourses)}
+
+        for post, pre in prerequisites:
+            adj[pre].append(post)
+            dep[post] += 1
+
+        q = collections.deque()
+
+        for i in range(numCourses):
+            if dep[i] == 0:
+                q.append(i)
+
+        while len(q) > 0:
+            course = q.popleft()
+            numCourses -= 1
+            for nei in adj[course]:
+                if dep[nei] == 0:
+                    continue
+                    
+                if dep[nei] > 1:
+                    dep[nei] -= 1
+                    continue
+                
+                q.append(nei)
+        
+        return numCourses <= 0
+
+class SolutionFailedToReadNumCoursesDuringRevision(object):
+    def canFinish(self, numCourses, prerequisites):
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :rtype: bool
+        """
+        
+        adj = {}
+        dep = {}
+
+        for post, pre in prerequisites:
+            if post not in adj:
+                adj[post] = []
+                dep[post] = 0
+            if pre not in adj:
+                adj[pre] = []
+                dep[pre] = 0
+            
+            adj[pre].append(post)
+            dep[post] += 1
+
+
+        completed = set()
+        q = collections.deque()
+        courses = 0
+
+        for key, deps in dep.items():
+            courses += 1
+            if deps == 0:
+                completed.add(key)
+                q.append(key)
+
+        while len(q) > 0:
+            course = q.popleft()
+            courses -= 1
+            for nei in adj[course]:
+                if nei in completed:
+                    continue
+                if dep[nei] > 1:
+                    dep[nei] -= 1
+                    continue
+                
+                completed.add(nei)
+                q.append(nei)
+        
+        return courses <= 0
+
+class SolutionBFSOG(object):
     def canFinish(self, numCourses, prerequisites):
         """
         :type numCourses: int
@@ -58,12 +142,12 @@ class SolutionBFS(object):
         for i in range(numCourses):
             if numPrereqs[i] == 0:
                 q.append(i)
-                numCourses -= 1
+                numCourses -= 1 # This one is reducing the courses as we add to queue (pre processing)
                 
         while q:
             current = q.popleft()
             for subsequent in adjList[current]:
-                numPrereqs[subsequent] -= 1
+                numPrereqs[subsequent] -= 1 # Unecessary work.
                 if numPrereqs[subsequent] == 0:
                     q.append(subsequent)
                     numCourses -= 1
