bd4fda6c3f2793696ad2379cd37aa586891e733b
vjohndo <v.john.do@outlook.com>
2023-04-13 00:26:22 +1000

    0028 revised

--- 0028-find-the-index-of-the-first-occurence-in-a-string/solution.py
+++ 0028-find-the-index-of-the-first-occurence-in-a-string/solution.py
@@ -5,6 +5,34 @@ class Solution(object):
         :type needle: str
         :rtype: int
         """
+        left = 0
+        right = len(needle) - 1
+
+        while right < len(haystack):
+            j = 0
+            k = left
+            while j < len(needle):
+                if needle[j] == haystack[k]:
+                    j += 1
+                    k += 1
+                else:
+                    break
+            
+            if k > right:
+                return left
+
+            left += 1
+            right += 1
+        
+        return -1
+
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
 
         i = 0
         while i <= len(haystack) - len(needle):
